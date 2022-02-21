d2ecbc0834be60ae94cb653f7b3ed38ce2a4ec89
Yorrel <yorrel@web.de>
2022-02-21 21:27:17 +0100

    update startup scripts

--- startup/blight-init.lua
+++ startup/blight-init.lua
@@ -1,7 +1,7 @@
--- add to $LUA_PATH: path_to_mg_lua/mg-lua/lua
--- add to $LUA_PATH: path_to_mg_custom/mg-custom/lua
+-- add to $LUA_PATH: path_to_mg_lua/mg-lua
+-- add to $LUA_PATH: path_to_mg_custom/mg-custom
 -- adapt and copy this file to $HOME/.config/blightmud/
 
 script.load('path_to_mg_lua/mg-lua/main.lua')
-script.load('path_to_mg_custom/mg-custom/init-custom.lua')
+script.load('path_to_mg_custom/mg-custom/main.lua')
 script.load('path_to_worlds_mg/worlds_mg.lua')
